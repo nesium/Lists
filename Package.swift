66dfd21523cf92c9105e30fc05196cdea11322ce
mb <mb@nesium.com>
2019-10-03 17:07:42 +0700

    updated dependencies

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.1")),
-    .package(url: "https://github.com/nesium/IGListKit.git", .branch("master")),
-    .package(url: "https://github.com/nesium/SwipeCellKit.git", .branch("swiftpm")), 
+    .package(url: "https://github.com/nesium/IGListKit.git", .upToNextMajor(from: "3.4.1")),
+    .package(url: "https://github.com/nesium/SwipeCellKit.git", .upToNextMajor(from: "1.9.2")),
     .package(url: "https://github.com/nesium/Bindings.git", .upToNextMajor(from: "1.0.0")),
     .package(url: "https://github.com/nesium/NSMUIKit.git", .upToNextMajor(from: "1.0.0")),
     .package(url: "https://github.com/nesium/NSMFoundation.git", .upToNextMajor(from: "1.0.0"))
